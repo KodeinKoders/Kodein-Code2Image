f9c815337c06405052235fcbcc3defb7dcfceb9d
Salomon BRYS <salomon.brys@gmail.com>
2021-03-16 13:06:53 +0100

    Added github deployment.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("js") version "1.4.31"
+    id("org.ajoberstar.git-publish") version "3.0.0"
+    id("org.ajoberstar.grgit") version "4.1.0"
 }
 
 group = "org.kodein.code2img"
@@ -28,5 +30,34 @@ kotlin {
         browser {
             binaries.executable()
         }
+    }
+}
+
+val auth = (project.findProperty("com.github.http.auth") as? String)?.split(":")
+if (auth != null) {
+    System.setProperty("org.ajoberstar.grgit.auth.username", auth[0])
+    System.setProperty("org.ajoberstar.grgit.auth.password", auth[1])
+}
+
+gitPublish {
+    repoUri.set("https://github.com/KodeinKoders/Kodein-Code2Image.git")
+    branch.set("gh-pages")
+    contents.apply {
+        from("$projectDir/build/distributions")
+    }
+    val head = grgit.head()
+    commitMessage.set("${head.abbreviatedId}: ${head.fullMessage}")
+}
+
+task("deployWebsiteToGithubPages") {
+    group = "publishing"
+    dependsOn("gitPublishPush")
+}
+
+tasks["gitPublishCopy"].dependsOn("browserDistribution")
+
+tasks["gitPublishCommit"].doFirst {
+    if (!grgit.status().isClean) {
+        error("Refusing to commit new pages on a non-clean repo. Please commit first.\n${grgit.status()}")
     }
 }
